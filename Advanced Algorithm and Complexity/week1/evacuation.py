b3f024f1c8160ebb81cd887117ba070c9fdbd584
aduo122 <ytarch122@gmail.com>
2018-04-18 17:44:21 -0400

    Add files via upload

--- Advanced Algorithm and Complexity/week1/evacuation.py
+++ Advanced Algorithm and Complexity/week1/evacuation.py
@@ -55,12 +55,53 @@ def read_data():
     for _ in range(edge_count):
         u, v, capacity = map(int, input().split())
         graph.add_edge(u - 1, v - 1, capacity)
+    # print(graph.get_edge(0).u,graph.get_edge(0).v,graph.get_edge(1).u,graph.get_edge(1).v)
+    # print(graph.get_edge(2).u,graph.get_edge(2).v,graph.get_edge(3).u,graph.get_edge(3).v)
+    # print(graph.get_edge(4).u, graph.get_edge(4).v, graph.get_edge(5).u, graph.get_edge(5).v)
     return graph
 
+def findpath(graph,from_,to):
+    queue = [from_]
+    dicpath = {}
+    #print(len(graph.graph))
+    #print(len(graph.edges))
+    while queue:
+        cur = queue.pop(0)
+        if cur == to:
+            path = []
+            min_flow = float("inf")
+            while cur != 0:
+                edge = graph.edges[dicpath[cur]]
+                min_flow = min(min_flow,edge.capacity)
+                #print(min_flow)
+                path.append(dicpath[cur])
+                cur = edge.u
+            return path,min_flow
+        for ind in graph.graph[cur]:
+            temp_edge = graph.edges[ind]
+            temp_to = temp_edge.v
+            #if temp_to not in dicpath and temp_edge.capacity > temp_edge.flow:
+            if temp_to not in dicpath and temp_edge.capacity > 0:
+                queue.append(temp_to)
+                dicpath[temp_to] = ind
+    return False,0
 
 def max_flow(graph, from_, to):
     flow = 0
     # your code goes here
+    path,min_flow = findpath(graph, from_, to)
+    #print(len(path),min_flow)
+
+    while path:
+        flow += min_flow
+        for id in path[::-1]:
+            edge = graph.get_edge(id)
+            edge.capacity -= min_flow
+            edge_reverse = graph.get_edge(id^1)
+            edge_reverse.capacity += min_flow
+
+            graph.add_flow(id, min_flow)
+        path, min_flow = findpath(graph, from_, to)
     return flow
 
 
