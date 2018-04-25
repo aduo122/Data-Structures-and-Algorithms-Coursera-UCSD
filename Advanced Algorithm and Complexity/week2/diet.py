98f908dd2bffd13ebdf6f71b7ba20eef495a5439
aduo122 <ytarch122@gmail.com>
2018-04-25 17:19:02 -0400

    Add files via upload

--- Advanced Algorithm and Complexity/week2/diet.py
+++ Advanced Algorithm and Complexity/week2/diet.py
@@ -1,24 +1,100 @@
 # python3
+
 from sys import stdin
-  
-def solve_diet_problem(n, m, A, b, c):  
-  # Write your code here
-  return [0, [0] * m]
+import numpy as np
+import itertools
 
-n, m = list(map(int, stdin.readline().split()))
+
+def run_solve(n, m, A, b, c):
+    numOfEquations = len(A)
+    equationGroups = list(itertools.combinations([i for i in range(n + m)], m))
+    maxValue = -float('inf')
+    ansx = np.array([])
+
+    for i in equationGroups:
+        m1 = []
+        m2 = []
+        for j in i:
+            m1.append(A[j])
+            m2.append(b[j])
+        try:
+            x = np.linalg.solve(np.array(m1), np.array(m2))
+        except:
+            continue
+        prerequisite = True
+        for k in range(numOfEquations):
+            if k in i or sum(x * np.array(A[k])) <= b[k]:
+                continue
+            else:
+                prerequisite = False
+                break
+        if prerequisite == False:
+            continue
+        value = sum(x * np.array(c))
+        if value > maxValue:
+            maxValue = value
+            ansx = x
+    return list(ansx)
+
+
+def solve_check(n, m, A, b, c, ansx):
+    numOfEquations = len(A)
+    equationGroups = list(itertools.combinations([i for i in range(n + m)], m))
+    maxValue = sum(np.array(ansx) * np.array(c))
+
+    for i in equationGroups:
+        if numOfEquations - 1 not in i:
+            continue
+            m1 = []
+            m2 = []
+        for j in i:
+            m1.append(A[j])
+            m2.append(b[j])
+        try:
+            x = np.linalg.solve(np.array(m1), np.array(m2))
+        except:
+            continue
+        prerequisite = True
+        for k in range(numOfEquations):
+            if k in i or sum(x * np.array(A[k])) <= b[k]:
+                continue
+            else:
+                prerequisite = False
+                break
+        if prerequisite == False:
+            continue
+
+        value = sum(x * np.array(c))
+        if value > maxValue:
+            maxValue = value
+            ansx = x
+    return list(ansx)
+
+
+n, m = list(map(int, input().split()))
 A = []
+for i in range(m):
+    unit_vector = [0 for i in range(m)]
+    unit_vector[i] = -1
+    A.append(unit_vector)
 for i in range(n):
-  A += [list(map(int, stdin.readline().split()))]
-b = list(map(int, stdin.readline().split()))
-c = list(map(int, stdin.readline().split()))
-
-anst, ansx = solve_diet_problem(n, m, A, b, c)
-
+    A += [list(map(int, input().split()))]
+b = [0 for i in range(m)] + list(map(int, input().split()))
+c = list(map(int, input().split()))
+ACheck = A + [[1 for i in range(m)]]
+bCheck = b + [10 ** 9]
+ansx = run_solve(n, m, A, b, c)
+anst = 0
+if len(ansx) == 0:
+    anst = -1
+elif ansx != solve_check(n + 1, m, ACheck, bCheck, c, ansx):
+    anst = 1
 if anst == -1:
-  print("No solution")
-if anst == 0:  
-  print("Bounded solution")
-  print(' '.join(list(map(lambda x : '%.18f' % x, ansx))))
+    print("No solution")
+if anst == 0:
+    print("Bounded solution")
+    for row in range(len(ansx)):
+        ansx[row] = "%.20lf" % ansx[row]
+    print(*ansx, sep=' ')
 if anst == 1:
-  print("Infinity")
-    
+    print("Infinity")
